d5ffbd68eb663f5794a0b1542349bf056a392b82
Kamysheva1723 <natalia.kamysheva@metropolia.fi>
2023-04-07 16:57:54 +0300

    Task 3

---
+++ Module 2/Task 3/task_3.js
@@ -0,0 +1,18 @@
+/* Write a program that asks for the names of six dogs.
+The program prints dog names to unordered list <ul>
+in reverse alphabetical order. (2p)
+*/
+"use strict"
+
+let dogs = [];
+
+for(let i=0; i<6; i++){
+  dogs[i] = prompt("Enter name of " + (i+1).toString() + ". dog...")
+}
+
+dogs.sort();
+dogs.reverse();
+
+for(let i=0; i<6; i++){
+  document.getElementById("dog_list").innerHTML += "<li>" + dogs[i] +"</li>"
+}
