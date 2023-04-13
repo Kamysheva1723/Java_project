4107fd671920aaaba900791cd0395ec1dedd0e6a
Kamysheva1723 <natalia.kamysheva@metropolia.fi>
2023-04-13 20:16:26 +0300

    Task 3

--- Module 3/module3-starters-main/t3/3.js
+++ Module 3/module3-starters-main/t3/3.js
@@ -1,2 +1,15 @@
+/*Open t3 folder in your IDE/editor. Add HTML by using innerHTML property. (2p)
+Add the following HTML code to the element with id="target". Add the values
+from 'names' array to the <li> elements in a for-loop.
+<li>John</li>
+<li>Paul</li>
+<li>Jones</li>*/
+
 'use strict';
 const names = ['John', 'Paul', 'Jones'];
+
+const target = document.getElementById("target");
+
+for (let i = 0; i < names.length; i++){
+ target.innerHTML += "<li>" + names[i] + "</li>";
+}
