816365658f5513e2669f574bd32d44d509d86316
Kamysheva1723 <natalia.kamysheva@metropolia.fi>
2023-04-25 10:00:53 +0300

    Task 7

--- Module 4/t7/t7.js
+++ Module 4/t7/t7.js
@@ -29,9 +29,21 @@ function getCoord(address) {
   fetch(url, fetchOptions).then(function(response) {
     return response.json();
   }).then(function(result) {
-    console.log(result.features[0].geometry.coordinates);
-    const resultCoordinates = {latitude: result.features[0].geometry.coordinates[1],longitude: result.features[0].geometry.coordinates[0]};
-    getRoute(resultCoordinates,{latitude: 60.22, longitude: 24.75})
+    try {
+      console.log("string 33" + result);
+      console.log(result.features[0].geometry.coordinates);
+      const resultCoordinates = {
+        latitude: result.features[0].geometry.coordinates[1],
+        longitude: result.features[0].geometry.coordinates[0]
+      };
+      getRoute(resultCoordinates, {latitude: 60.22, longitude: 24.75})
+    }
+    catch {
+          const next_route = document.createElement("h5")
+          next_route.textContent = "Can't find location...";
+          document.getElementById("result").appendChild(next_route);
+        }
+
     })
 
 }
@@ -74,44 +86,54 @@ function getRoute(origin, target) {
     fetch(apiAddress, fetchOptions).then(function (response) {
         return response.json();
     }).then(function (result) {
-        console.log(result.data.plan.itineraries[0].legs);
-        const googleEncodedRoute = result.data.plan.itineraries[0].legs;
+        if (result.data.plan.itineraries.length != 0) {
+          console.log("string 89 result.data.plan.itineraries.length" + result.data.plan.itineraries.length);
+          console.log(result.data.plan.itineraries[0].legs);
+          const googleEncodedRoute = result.data.plan.itineraries[0].legs;
 
-        const startTime = googleEncodedRoute[0].startTime;
-        const endTime = googleEncodedRoute[googleEncodedRoute.length-1].endTime;
-        const next_route = document.createElement("h5")
-        next_route.textContent = "Start time: "
-            + getTime(startTime) + "; end time: " + getTime(endTime);
-        document.getElementById("result").appendChild(next_route);
+          const startTime = googleEncodedRoute[0].startTime;
+          const endTime = googleEncodedRoute[googleEncodedRoute.length -
+          1].endTime;
+          const next_route = document.createElement("h5")
+          next_route.textContent = "Start time: "
+              + getTime(startTime) + "; end time: " + getTime(endTime);
+          document.getElementById("result").appendChild(next_route);
 
-        for (let i = 0; i < googleEncodedRoute.length; i++) {
+          for (let i = 0; i < googleEncodedRoute.length; i++) {
             let color = '';
             switch (googleEncodedRoute[i].mode) {
-                case 'WALK':
-                    color = 'green';
-                    break;
-                case 'BUS':
-                    color = 'red';
-                    break;
-                case 'RAIL':
-                    color = 'cyan'
-                    break;
-                case 'TRAM':
-                    color = 'magenta'
-                    break;
-                default:
-                    color = 'blue';
-                    break;
+              case 'WALK':
+                color = 'green';
+                break;
+              case 'BUS':
+                color = 'red';
+                break;
+              case 'RAIL':
+                color = 'cyan'
+                break;
+              case 'TRAM':
+                color = 'magenta'
+                break;
+              default:
+                color = 'blue';
+                break;
             }
             const route = (googleEncodedRoute[i].legGeometry.points);
             const pointObjects = L.Polyline.fromEncoded(route).getLatLngs(); // fromEncoded: convert Google encoding to Leaflet polylines
             L.polyline(pointObjects).setStyle({
-                color
+              color
             }).addTo(map);
+          }
+
+          map.fitBounds([
+            [origin.latitude, origin.longitude],
+            [target.latitude, target.longitude]]);
         }
-
-        map.fitBounds([[origin.latitude, origin.longitude], [target.latitude, target.longitude]]);
-
+        else {
+          const next_route = document.createElement("h5")
+          next_route.textContent = "Can't find route...";
+          document.getElementById("result").appendChild(next_route);
+        }
     }).catch(function (e) {
         console.error(e.message);
     });
